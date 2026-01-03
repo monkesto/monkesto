4f610e4c6477ce6d6dbfc0353c3972ce20e6980f
shaggysa <gabriel.couchenour@gmail.com>
2026-01-03 13:01:44 -0600

    fix stylesheet href and install node in the dockerfile

--- src/maud_header.rs
+++ src/maud_header.rs
@@ -7,7 +7,7 @@ pub fn header(content: Markup) -> Markup {
             head {
                 meta charset="UTF-8";
                 meta name="viewport" content="width=device-width, initial-scale=1.0";
-                link rel="stylesheet" href="/pkg/monkesto.css";
+                link rel="stylesheet" href="/monkesto.css";
             }
             body {
                 (content)
