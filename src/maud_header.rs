078fa4940c280891c743642871950264926ff461
Ryan Hiebert <ryan@ryanhiebert.com>
2026-01-03 07:22:07 -0600

    Skip compiling tailwind
    Add back the head and body elements and the meta tags to make the design
    responsive.

--- src/maud_header.rs
+++ src/maud_header.rs
@@ -3,9 +3,15 @@ use maud::{DOCTYPE, Markup, html};
 pub fn header(content: Markup) -> Markup {
     html! {
         (DOCTYPE)
-        html class="h-full bg-white dark:bg-gray-900 text-gray-900 dark:text-white"{
-            link rel="stylesheet" href="/pkg/monkesto.css";
-            (content)
+        html class="h-full bg-white dark:bg-gray-900 text-gray-900 dark:text-white" {
+            head {
+                meta charset="UTF-8";
+                meta name="viewport" content="width=device-width, initial-scale=1.0";
+                script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4" {}
+            }
+            body {
+                (content)
+            }
         }
     }
 }
