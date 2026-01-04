edec132977c0e352df03a6636d38b22896264be1
Ryan Hiebert <ryan@ryanhiebert.com>
2026-01-03 21:06:00 -0600

    webauthn module cleanups

--- src/webauthn/mod.rs
+++ src/webauthn/mod.rs
@@ -1,3 +1,7 @@
+mod auth;
+mod error;
+mod startup;
+
 use axum::{
     Router,
     extract::Extension,
@@ -12,63 +16,31 @@ use tower_sessions::{
     cookie::{SameSite, time::Duration},
 };
 
-mod error;
-/*
- * Webauthn RS server side tutorial.
- */
-
-// The handlers that process the data can be found in the auth.rs file
-// This file contains the wasm client loading code and the axum routing
 use auth::{finish_authentication, finish_register, start_authentication, start_register};
 use startup::AppState;
 
-// Moved to src/main.rs
-//
-// #[macro_use]
-// extern crate tracing;
-
-mod auth;
-mod startup;
-
-// 7. That's it! The user has now authenticated!
-
-// =======
-// Below is glue/stubs that are needed to make the above work, but don't really affect
-// the work flow too much.
+use crate::maud_header::header;
 
 pub fn router<S: Clone + Send + Sync + 'static>() -> Router<S> {
-    // Create the app
-    let app_state = AppState::new();
-
-    let session_store = MemoryStore::default();
-
-    // build our application with a route
-    let app = Router::new()
+    Router::new()
         .route("/register_start/{username}", post(start_register))
         .route("/register_finish", post(finish_register))
         .route("/login_start/{username}", post(start_authentication))
         .route("/login_finish", post(finish_authentication))
         .route("/", get(serve_auth_html))
         .route("/auth.js", get(serve_auth_js))
-        .layer(Extension(app_state))
+        .layer(Extension(AppState::new()))
         .layer(
-            SessionManagerLayer::new(session_store)
+            SessionManagerLayer::new(MemoryStore::default())
                 .with_name("webauthnrs")
                 .with_same_site(SameSite::Strict)
                 .with_secure(false) // TODO: change this to true when running on an HTTPS/production server instead of locally
                 .with_expiry(Expiry::OnInactivity(Duration::seconds(360))),
         )
-        .fallback(handler_404);
-
-    Router::new().merge(app)
-}
-
-async fn handler_404() -> impl IntoResponse {
-    (StatusCode::NOT_FOUND, "nothing to see here")
 }
 
 fn auth_page() -> Markup {
-    html! {
+    header(html! {
         (DOCTYPE)
         html lang="en" {
             head {
@@ -98,7 +70,7 @@ fn auth_page() -> Markup {
                 }
             }
         }
-    }
+    })
 }
 
 async fn serve_auth_html() -> impl IntoResponse {
